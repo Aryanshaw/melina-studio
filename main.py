dc3f44eccafb6e47085127a80bcf1d7d4108a2cb
aryanShaw <aryanrot234@gmail.com>
2025-04-11 23:42:11 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import sys
+import os
+from contextlib import asynccontextmanager
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+import __init__ as project_root
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_routes()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+def init_routes():
+    print("Initializing routes")
+
+if __name__ == "main":
+    project_root
